f41059e50b512a5a8219e6fa94db1982e8ba8826
Balor.LC3 <supernova20315@gmail.com>
2026-04-05 11:42:12 -0700

    Created infraestructure

---
+++ gateway/internal/client/python_client.go
@@ -0,0 +1,36 @@
+package client
+
+import (
+	"context"
+
+	gen "github.com/BalorLC3/imprimer/gateway/gen"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+// PythonClient wraps the generated gRPC stub
+type PythonClient struct {
+	conn *grpc.ClientConn
+	stub gen.PromptEngineClient
+}
+
+func NewPythonClient(addr string) (*PythonClient, error) {
+	// insecure.NewCredentials means no tls, which is fine for Docker
+	conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+	return &PythonClient{
+		conn: conn,
+		stub: gen.NewPromptEngineClient(conn),
+	}, nil
+}
+
+// Call sends an EvaluateRequest to the Python engine and returns a response
+func (c *PythonClient) Call(ctx context.Context, req *gen.EvaluateRequest) (*gen.EvaluateResponse, error) {
+	return c.stub.EvaluatePrompt(ctx, req)
+}
+
+func (c *PythonClient) Close() error {
+	return c.conn.Close()
+}
